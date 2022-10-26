9bc5c08573bf1aa61140173aef89f98f7a8d0cee
giorgip62 <giorgiop62@gmail.com>
2022-10-26 15:53:59 +0200

    insert img

--- js/script.js
+++ js/script.js
@@ -2,3 +2,39 @@ const slider = document.querySelector('.slider');
 const thumbs = document.querySelector('.thumbs');
 const container = document.querySelector('.container');
 
+const img = [
+  {
+    immagine:"http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg",
+    luogo: "Svezia",
+    descrizione:"ibra è nato in Svezia"
+
+  },
+  {
+    immagine:"https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg",
+    luogo: "Peru",
+    descrizione:"Viva il peru"
+
+  },
+  {
+    immagine:"https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c",
+    luogo: "Chile",
+    descrizione:"Chile is nice"
+
+  },
+  {
+    immagine:"https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg",
+    luogo: "Argentina",
+    descrizione:"Ho tanti amici Argentini"
+
+  },
+  {
+    immagine:"https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop",
+    luogo: "Colombia",
+    descrizione:"Colombia è top"
+
+  },
+
+];
+
+
+
